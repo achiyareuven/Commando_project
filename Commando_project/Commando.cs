49fb658a1a58c2e805d34c37e0f8c24f7d0133d7
achiya reuven <achiya3144@gmail.com>
2025-05-27 11:50:28 +0300

    I built a commando class and a weapon class.

---
+++ Commando_project/Commando.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commando_project
+{
+    enum Status
+    {
+        Standing,
+        Walking,
+        Hiding
+    }
+    internal class Commando
+    {
+        public string _name { get; set; }
+        public string _codeName { get; set; }
+        private string[] _tools = { "Hammer, chisel, rope, bag, water bottle"};
+        private Status _currentstatus;
+
+        public Commando (string name, string codeName)
+        {
+            _name = name;
+            _codeName = codeName;
+            _currentstatus = Status.Standing;
+        }
+
+        public void Walk()
+        {
+            Console.WriteLine($"{_name}; is {Status.Walking} ");
+            _currentstatus = Status.Walking;
+        }
+        public void Hide()
+        {
+            _currentstatus =Status.Hiding;
+            Console.WriteLine($" {_name} is {_currentstatus}");
+        }
+        public void Attack()
+        {
+            Console.WriteLine($"{_name} is attacks");
+        }
+    }
+}
---
+++ Commando_project/Weapon.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commando_project
+{
+    internal class Weapon
+    {
+        public string _name {  get; set; }
+        public string _manufacturer {  get; set; }
+        public int _amountOfBalls { get; set; }
+
+
+        public Weapon(string name ,string manufacturer,int amountOfBalls)
+        {
+            _name = name;
+            _manufacturer = manufacturer;
+            _amountOfBalls = amountOfBalls;
+        }
+
+        public void Shoot()
+        {
+            if (_amountOfBalls > 0)
+            {
+                Console.WriteLine("A shot was fired.");
+                _amountOfBalls--;
+            }
+            else
+            {
+                Console.WriteLine("Not enough bullets");
+            }
+        }
+        
+
+
+
+    }
+}
